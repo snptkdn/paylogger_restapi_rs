bf6386901447794419b3fc3c68525be2d7c7b9ae
snptkdn <400m.wflick@gmail.com>
2022-12-04 12:32:02 +0900

    dbクラスを追加した。

---
+++ src/services/db.rs
@@ -0,0 +1,23 @@
+use sqlx::mysql::MySqlPoolOptions;
+use sqlx::{Pool, MySql};
+use std::env;
+use std::sync::Arc;
+
+#[derive(Clone)]
+pub struct Db(pub(crate) Arc<Pool<MySql>>);
+
+impl Db {
+    pub async fn new() -> Db {
+        let pool = MySqlPoolOptions::new()
+            .max_connections(8)
+            .connect(
+                &env::var("DATABASE_URL").unwrap_or_else(|_| panic!("DATABASE_URL must be set!")),
+            )
+            .await
+            .unwrap_or_else(|_| {
+                panic!("Cannot connect to the database. Please check your configuration.")
+            });
+
+        Db(Arc::new(pool))
+    }
+}
