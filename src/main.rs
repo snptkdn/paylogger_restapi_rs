5aadb7ebaef612a15cba8201e34c113c3f63e659
snptkdn <400m.wflick@gmail.com>
2023-02-12 15:08:36 +0900

    CORSを可能にした。

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 #![feature(proc_macro_hygiene)]
 #![feature(decl_macro)]
+
+use rocket::response::status;
+use rocket_cors::{AllowedOrigins, CorsOptions};
+use rocket::http::Method;
+
 #[macro_use]
 extern crate rocket;
 
@@ -7,9 +12,26 @@ mod models;
 mod services;
 mod controllers;
 
+#[get("/")]
+pub fn root() -> status::Accepted<String> {
+    status::Accepted(Some("Welcome to paylogger!".to_string()))
+}
+
 fn main() {
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::all())
+        .allowed_methods(
+            vec![Method::Get, Method::Post, Method::Patch]
+                .into_iter()
+                .map(From::from)
+                .collect(),
+        )
+        .allow_credentials(true);
+
     rocket::ignite()
-        .mount("/", routes![controllers::log_controllers::index])
+        .attach(cors.to_cors().unwrap())
+        .mount("/", routes![root])
+        .mount("/log", routes![controllers::log_controllers::index])
         .mount("/log", routes![controllers::log_controllers::post_new_log])
         .mount("/log", routes![controllers::log_controllers::per_category])
         .mount("/log", routes![controllers::log_controllers::per_day])
